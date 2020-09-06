c4d4bfdee17a67a181c38bd288379b79b76a1540
Mika Vakula <7551056+mvakula@users.noreply.github.com>
2020-09-07 02:28:42 +0300

    Add rules

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  plugins:[
+    "@typescript-eslint",
+    "prettier",
+  ],
   rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true,
+        "semi": false,
+        "tabWidth": 2,
+        "trailingComma": "none",
+        "arrowParens": "avoid"
+      }
+    ],
+    "@typescript-eslint/no-floating-promises": 2,
+    "@typescript-eslint/no-submodule-imports": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/no-use-before-define": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/ban-ts-ignore": 0,
+    "@typescript-eslint/camelcase": [
+      "error",
+      {
+        "properties": "never"
+      }
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "varsIgnorePattern": "^_",
+        "argsIgnorePattern": "^_"
+      }
+    ],
       'no-shadow': 'error',
       'no-console': 'error'
   }
