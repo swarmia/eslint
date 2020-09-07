5af226ef3b697d1eb944b3ef9d3a3be6b8506124
Mika Vakula <7551056+mvakula@users.noreply.github.com>
2020-09-07 09:33:03 +0300

    Add eslint to this project as well

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
-  plugins:[
-    "@typescript-eslint",
-    "prettier",
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        "singleQuote": true,
-        "semi": false,
-        "tabWidth": 2,
-        "trailingComma": "none",
-        "arrowParens": "avoid"
+        singleQuote: true,
+        semi: false,
+        tabWidth: 2,
+        trailingComma: 'none',
+        arrowParens: 'avoid'
       }
     ],
-    "@typescript-eslint/no-floating-promises": 2,
-    "@typescript-eslint/no-submodule-imports": 0,
-    "@typescript-eslint/no-non-null-assertion": 0,
-    "@typescript-eslint/no-use-before-define": 0,
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/ban-ts-ignore": 0,
-    "@typescript-eslint/camelcase": [
-      "error",
+    '@typescript-eslint/no-floating-promises': 2,
+    '@typescript-eslint/no-submodule-imports': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/camelcase': [
+      'error',
       {
-        "properties": "never"
+        properties: 'never'
       }
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        "varsIgnorePattern": "^_",
-        "argsIgnorePattern": "^_"
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_'
       }
     ],
-      'no-shadow': 'error',
-      'no-console': 'error'
+    'no-shadow': 'error',
+    'no-console': 'error'
   }
 }
